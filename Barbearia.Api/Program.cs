0f99bd92724b6125fcf622898b431659f36ceaae
João Paulo Cardoso D'Oliveira <65909583+JoPCardoso2203@users.noreply.github.com>
2023-10-19 18:14:02 -0300

    Update Program.cs
    Ajuste CORS

--- Barbearia.Api/Program.cs
+++ Barbearia.Api/Program.cs
@@ -28,8 +28,12 @@ namespace Barbearia.Api
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
 
+            app.UseAuthorization();
 
             app.MapControllers();
 
